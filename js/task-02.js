20eb5d26b89e0d329297514557728bcf12fbe6cb
Bellmere <therise00@gmail.com>
2022-10-26 23:26:14 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredients.forEach((item, index) => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = `${item}`;
+  ingredientsEl.append(itemEl);
+});
+
+console.log(ingredientsEl);
