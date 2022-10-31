048ea987d75062ec8b0aac4b6197d45bd177da80
Bellmere <therise00@gmail.com>
2022-10-31 11:27:58 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
-const inputEl = document.querySelector('#font-size-control');
+const inputEl = document.querySelector('input#font-size-control');
 const spanEl = document.querySelector('span#text');
 console.log(spanEl);
+console.log(inputEl);
 
 
-inputEl.addEventListener('input', e => {
-    spanEl.style.fontSize = `${e.target.value} px`;
+inputEl.addEventListener('input', element => {
+    spanEl.style.fontSize = `${Number(element.currentTarget.value)}px`;
 });
