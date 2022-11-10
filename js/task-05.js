0d4c033531d33fba52702f62d4dfacefbd43e774
Bellmere <therise00@gmail.com>
2022-11-10 12:02:03 +0200

    update logic

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const spanEl = document.querySelector('#name-output');
 inputEl.addEventListener ('input', (event) => {
     spanEl.textContent = event.target.value;
 
-    if (event.target.value === '') {
+    if (!event.target.value) {
         spanEl.textContent = 'Anonymous';
     }
 });
--- js/task-08.js
+++ js/task-08.js
@@ -7,7 +7,7 @@ function onSubmit(event) {
         elements: { email, password },
     } = event.currentTarget;
 
-    if (email.value === '' || password.value === '') {
+    if (!email.value || !password.value) {
         return alert('All fields must be filled');
     }
 
